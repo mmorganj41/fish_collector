2642c719178f75c13220517afc2507adbdfe05f4
mmorganj41 <mmorganj41@gmail.com>
2022-12-21 14:17:37 -0500

    create fish index and pseudomodel

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 
+class Fish():
+	def __init__(self, name, species, description, age):
+		self.name = name
+		self.species = species
+		self.description = description
+		self.age = age
+
+fish = [
+	Fish('Nemo', 'clownfish', 'adventurous mongrel', 0),
+	Fish('Pokey', 'swordfish', 'loves to swordfight', 5),
+	Fish('Fido', 'parrotfish', 'copies everything you say', 7),
+]
+
 def home(request):
 	return render(request, 'home.html')
 
 def about(request):
 	return render(request, 'about.html')
+
+def fish_index(request):
+	return render(request, 'fish/index.html', {'fish': fish})
