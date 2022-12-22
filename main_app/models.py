3bcdd4328d71f3f489dd0e6b2a94c5b49b2af52e
mmorganj41 <mmorganj41@gmail.com>
2022-12-22 11:41:39 -0500

    fish model create

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Fish(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
