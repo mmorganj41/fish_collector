e7215ee52a1518c16113cd011d375326ec2a6e61
mmorganj41 <mmorganj41@gmail.com>
2022-12-22 12:02:46 -0500

    create detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 	path('', views.home, name='home'),
 	path('about/', views.about, name='about'),
-	path('fish/', views.fish_index, name='fish')
+	path('fish/', views.fish_index, name='fish'),
+	path('fish/<int:f_id>', views.fish_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,8 @@
 from django.shortcuts import render
+from .models import Fish
 
 # Create your views here.
 
-class Fish():
-	def __init__(self, name, species, description, age):
-		self.name = name
-		self.species = species
-		self.description = description
-		self.age = age
-
-fish = [
-	Fish('Nemo', 'clownfish', 'adventurous mongrel', 0),
-	Fish('Pokey', 'swordfish', 'loves to swordfight', 5),
-	Fish('Fido', 'parrotfish', 'copies everything you say', 7),
-]
 
 def home(request):
 	return render(request, 'home.html')
@@ -22,4 +11,9 @@ def about(request):
 	return render(request, 'about.html')
 
 def fish_index(request):
+	fish = Fish.objects.all()
 	return render(request, 'fish/index.html', {'fish': fish})
+
+def fish_detail(request, f_id):
+	f = Fish.objects.get(id=f_id)
+	return render(request, 'fish/detail.html', {'f': f})
