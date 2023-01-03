85dfcf451ab78b19f0f7673daa029f63413cd8d0
mmorganj41 <mmorganj41@gmail.com>
2023-01-03 15:08:11 -0500

    register toy admin site

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Fish, Feeding
+from .models import Fish, Feeding, Toy
 
 admin.site.register(Fish)
 admin.site.register(Feeding)
+admin.site.register(Toy)
